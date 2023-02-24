0bf133bd37ab7f8575dcfe70c6e272315fb326d1
Oscar Camplin <oscarcamplin@microsoft.com>
2023-02-25 11:14:55 +1300

    Add terraform provisioning helper script

---
+++ tf.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+apply(){
+  echo "Applying $1..."
+  cd "terraform/$1" || return
+  terraform init 
+  terraform plan -out outfile
+  terraform apply -auto-approve
+}
+
+destroy(){
+  echo "Destroying $1..."
+  cd "terraform/$1" || return
+  terraform destroy -auto-approve
+}
+
+cmd=$1
+
+base_dir=$(pwd)
+
+if [ "$cmd" = "apply" ];
+then
+    apply "networking"
+    cd $base_dir || return
+    apply "instance"
+  elif [ "$cmd" = "destroy" ] 
+then
+    destroy "instance"
+    cd $base_dir || return
+    destroy "networking"
+else
+    echo "Please provide arg (apply or destroy)"
+fi
+
+
